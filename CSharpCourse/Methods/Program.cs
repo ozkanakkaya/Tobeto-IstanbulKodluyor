81e8f425395bc6d087734ab34be0ef56957a4774
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 01:15:02 +0300

    Params keywod ile çalışma yapıldı.

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -13,6 +13,8 @@ Console.WriteLine(Multiply(2, 4));
 Console.WriteLine(Multiply1(2, 4, 5));
 
 
+Console.WriteLine(Add4(1, 2, 3, 4, 5, 6));
+
 Console.ReadLine();
 
 static void Add()
@@ -20,7 +22,7 @@ static void Add()
     Console.WriteLine("Added!");
 }
 
-static int Add2(int number1, int number2=30)//default değerler en sonda olmalı
+static int Add2(int number1, int number2 = 30)//default değerler en sonda olmalı
 {
     var result = number1 + number2;
     return result;
@@ -29,7 +31,7 @@ static int Add2(int number1, int number2=30)//default değerler en sonda olmalı
 static int Add3(out int number1, int number2)
 {
     number1 = 30;
-    return  number1 + number2;
+    return number1 + number2;
 
 }
 
@@ -41,4 +43,9 @@ static int Multiply(int number1, int number2)
 static int Multiply1(int number1, int number2, int number3)
 {
     return number1 * number2 * number3;
+}
+
+static int Add4(int number, params int[] numbers) // params sonda olmalı
+{
+    return numbers.Sum();
 }
