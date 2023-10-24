fec60b405fd4e5b67d0134eee38f68a265dbc597
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 00:34:29 +0300

    İç içe if bloglarıyla çalışma yapıldı

--- CSharpCourse/Conditionals/Program.cs
+++ CSharpCourse/Conditionals/Program.cs
@@ -1,4 +1,4 @@
-﻿var number = 201;
+﻿var number = 89;
 //if (number == 10)
 //{
 //    Console.WriteLine("Number is 10");
@@ -29,17 +29,30 @@
 //}
 
 
-if(number>=0&&number<=100)
+//if(number>=0&&number<=100)
+//{
+//    Console.WriteLine("Number is between 0-100");
+//}
+//else if (number > 100 && number <=200)
+//{
+//    Console.WriteLine("Number is between 101-200");
+//}
+//else if (number > 200 || number <0)
+//{
+//    Console.WriteLine("Number is less than 0 or greater than 200");
+//}
+
+
+if (number<100)
 {
-    Console.WriteLine("Number is between 0-100");
-}
-else if (number > 100 && number <=200)
-{
-    Console.WriteLine("Number is between 101-200");
-}
-else if (number > 200 || number <0)
-{
-    Console.WriteLine("Number is less than 0 or greater than 200");
+	if (number>=90&&number<95)
+	{
+		Console.WriteLine("Number is between 89-95");
+    }
+	else 
+	{
+        Console.WriteLine("Number is not between 89-95");
+    }
 }
 
 Console.ReadLine();
