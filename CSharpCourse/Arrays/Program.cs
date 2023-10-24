420ed55d3a2437fcc314e5596ebef0c9aeda8963
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 01:38:52 +0300

    Çok boyutlu diziler

--- CSharpCourse/Arrays/Program.cs
+++ CSharpCourse/Arrays/Program.cs
@@ -2,10 +2,27 @@
 string[] studens2 = { "Özkan", "Özkan2", "Özkan3" };
 //studens2[3] = "Ahmet";
 
-foreach (var student in studens2)
+//foreach (var student in studens2)
+//{
+//    Console.WriteLine(student);
+//}
+
+string[,] regions = new string[5, 3]
 {
-    Console.WriteLine(student);
+    {"İstanbul","İzmit","Balıkesir" },
+    {"Ankara","Konya","Kırıkkale" },
+    {"Antalya","Adana","Mersin" },
+    {"Rize","Trabzon","Samsun" },
+    {"İzmir","Muğla","Manisa" },
+};
+
+for (int i = 0; i <= regions.GetUpperBound(0); i++)
+{
+    for (int j = 0; j <= regions.GetUpperBound(1); j++)
+    {
+        Console.WriteLine(regions[i, j]);
+    }
+    Console.WriteLine("***************");
 }
 
-Console.WriteLine();
 Console.ReadLine();
