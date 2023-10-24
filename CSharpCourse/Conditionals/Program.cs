32ca5b9e64de9c6585c5ebe9489dfc9d97ffe9e3
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 00:23:36 +0300

    Demo: Çoklu şartlarla çalışma yapıldı

--- CSharpCourse/Conditionals/Program.cs
+++ CSharpCourse/Conditionals/Program.cs
@@ -1,4 +1,4 @@
-﻿var number = 11;
+﻿var number = 201;
 //if (number == 10)
 //{
 //    Console.WriteLine("Number is 10");
@@ -15,17 +15,31 @@
 //Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10 or 20");
 
 
-switch (number)
+//switch (number)
+//{
+//    case 10:
+//        Console.WriteLine("Number is 10");
+//        break;
+//    case 20:
+//        Console.WriteLine("Number is 20");
+//        break;
+//    default:
+//        Console.WriteLine("Number is not 10 or 20");
+//        break;
+//}
+
+
+if(number>=0&&number<=100)
 {
-    case 10:
-        Console.WriteLine("Number is 10");
-        break;
-    case 20:
-        Console.WriteLine("Number is 20");
-        break;
-    default:
-        Console.WriteLine("Number is not 10 or 20");
-        break;
+    Console.WriteLine("Number is between 0-100");
+}
+else if (number > 100 && number <=200)
+{
+    Console.WriteLine("Number is between 101-200");
+}
+else if (number > 200 || number <0)
+{
+    Console.WriteLine("Number is less than 0 or greater than 200");
 }
 
 Console.ReadLine();
