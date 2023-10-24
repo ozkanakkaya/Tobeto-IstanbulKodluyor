2e60994896236bdd3f6c03e60f8d8540cc32066f
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 00:46:40 +0300

    Parametreli metotlarla çalışma yapıldı.

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -1,9 +1,17 @@
 ﻿Add();
 Add();
+var result = Add2(10, 30);
+Console.WriteLine(result);
 
 Console.ReadLine();
 
 static void Add()
 {
     Console.WriteLine("Added!");
+}
+
+static int Add2(int number1, int number2)
+{
+    var result = number1 + number2;
+    return result;
 }
