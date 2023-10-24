c806e9c38a99c23f36de75b2aaf0f021d3ab99c1
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-24 17:42:07 +0300

    Var keyword ile çalışma yapıldı

--- CSharpCourse/TypesAndVariables/Program.cs
+++ CSharpCourse/TypesAndVariables/Program.cs
@@ -6,6 +6,9 @@ bool condition = true;// değer tiptedir.
 char character = 'A';
 double number5 = 10.4;//değer tiptedir. ondalıkla sayıları tutar, 64bit
 decimal number6 = 10.4m;//hassas verileri tutar, virgüllüyse m harfi yazılır, virgülden sonra en fazla 29 basamak, 8bit
+var number7 = 10;
+number7 = 'A';//ascii yani inttir
+//number7 = "A";//hata verecektir
 
 Console.WriteLine("Number1 is {0}", number1);
 Console.WriteLine("Number2 is {0}", number2);
@@ -14,6 +17,8 @@ Console.WriteLine("Number4 is {0}", number4);
 Console.WriteLine("Character is {0}", (int)character);//int'e çevirildi ve ascii kodunu verdi.
 Console.WriteLine("Number5 is {0}", number5);
 Console.WriteLine("Number6 is {0}", number6);
+Console.WriteLine("Number7 is {0}", number7);
+
 Console.WriteLine(Days.Friday);
 Console.WriteLine((int)Days.Friday);
 
