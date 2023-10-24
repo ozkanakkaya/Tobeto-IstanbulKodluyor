15c320e106f7741e5f2a601dc979965f2c347970
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-24 18:07:44 +0300

    Conditionals projesi oluşturuldu. If blogları, Single Line If ile çalışıldı.

---
+++ CSharpCourse/Conditionals/Program.cs
@@ -0,0 +1,13 @@
+﻿var number = 11;
+if (number == 10)
+{
+    Console.WriteLine("Number is 10");
+}
+else
+{
+    Console.WriteLine("Number is not 10");
+}
+
+Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+
+Console.ReadLine();
