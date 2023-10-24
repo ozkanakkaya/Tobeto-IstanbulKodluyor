4cd635eee17e38b4440fba100ba630d58000a8e0
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 00:11:31 +0300

    Else if bloğuyla çalışmak

--- CSharpCourse/Conditionals/Program.cs
+++ CSharpCourse/Conditionals/Program.cs
@@ -3,11 +3,15 @@ if (number == 10)
 {
     Console.WriteLine("Number is 10");
 }
+else if (number == 20)
+{
+    Console.WriteLine("Number is 20");
+}
 else
 {
-    Console.WriteLine("Number is not 10");
+    Console.WriteLine("Number is not 10 or 20");
 }
 
-Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10 or 20");
 
 Console.ReadLine();
