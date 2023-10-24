778d64240f4b1c5ff55d9011b46ca7ac5603e1b1
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-24 16:38:55 +0300

    Byte veri tipi

--- CSharpCourse/TypesAndVariables/Program.cs
+++ CSharpCourse/TypesAndVariables/Program.cs
@@ -1,9 +1,11 @@
 ﻿int number1 = 2147483647; // int veri tipinin değer aralığı +-2147483647, 32bit yer kaplar
 long number2 = 9223372036854775807;// int veri tipinin iki katı değer aralığı +-, 64bit yer kaplar
 short number3 = -32768;//32767 ve -32768, 16bit
+byte number4 = 255; //0-255 arası değer alıyor, 32bit
 
 Console.WriteLine("Number1 is {0}", number1);
 Console.WriteLine("Number2 is {0}", number2);
 Console.WriteLine("Number3 is {0}", number3);
+Console.WriteLine("Number4 is {0}", number4);
 
 Console.ReadLine();
