d3a7292767e24dd8e1de896a84dfccbe633cb57c
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 01:57:23 +0300

    Do while dönggüsü

--- CSharpCourse/Loops/Program.cs
+++ CSharpCourse/Loops/Program.cs
@@ -1,17 +1,40 @@
-﻿//for (int i = 100; i >= 0; i=i-2)
-//{
-//    Console.WriteLine(i);
-//}
-//Console.WriteLine("Finished!");
+﻿
+//ForLoop();
+//WhileLoop();
+DoWhileLoop();
 
-//Console.ReadLine();
 
-int number = 100;
-while (number>=0)
+static void WhileLoop()
 {
-    Console.WriteLine(number);
-    number--;
-}
-Console.WriteLine("Now number is {0}!", number);
+    int number = 100;
+    while (number >= 0)
+    {
+        Console.WriteLine(number);
+        number--;
+    }
+    Console.WriteLine("Now number is {0}!", number);
 
-Console.ReadLine();
+    Console.ReadLine();
+}
+
+static void ForLoop()
+{
+    for (int i = 100; i >= 0; i = i - 2)
+    {
+        Console.WriteLine(i);
+    }
+    Console.WriteLine("Finished!");
+
+    Console.ReadLine();
+}
+
+static void DoWhileLoop()
+{
+    int number = 100;
+    do
+    {
+        Console.WriteLine(number);
+        number--;
+    } while (number >= 11);
+    Console.ReadLine();
+}
