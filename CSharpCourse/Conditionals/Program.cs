6d5756d410a9c3ba7d2aa92affcd1978f363f51a
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 00:17:16 +0300

    Switch bloğuyla çalışma konusu işlendi

--- CSharpCourse/Conditionals/Program.cs
+++ CSharpCourse/Conditionals/Program.cs
@@ -1,17 +1,31 @@
 ﻿var number = 11;
-if (number == 10)
-{
-    Console.WriteLine("Number is 10");
-}
-else if (number == 20)
-{
-    Console.WriteLine("Number is 20");
-}
-else
-{
-    Console.WriteLine("Number is not 10 or 20");
-}
+//if (number == 10)
+//{
+//    Console.WriteLine("Number is 10");
+//}
+//else if (number == 20)
+//{
+//    Console.WriteLine("Number is 20");
+//}
+//else
+//{
+//    Console.WriteLine("Number is not 10 or 20");
+//}
 
-Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10 or 20");
+//Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10 or 20");
+
+
+switch (number)
+{
+    case 10:
+        Console.WriteLine("Number is 10");
+        break;
+    case 20:
+        Console.WriteLine("Number is 20");
+        break;
+    default:
+        Console.WriteLine("Number is not 10 or 20");
+        break;
+}
 
 Console.ReadLine();
