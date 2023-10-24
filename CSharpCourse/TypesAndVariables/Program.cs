b5c12dacea72d4e9c3e02dc78c814578b723f2db
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-24 16:29:14 +0300

    Long veri tipi

--- CSharpCourse/TypesAndVariables/Program.cs
+++ CSharpCourse/TypesAndVariables/Program.cs
@@ -1,4 +1,6 @@
-﻿int number1 = 2147483647; // int veri tipinin değer aralığı +-2147483647 32bit yer kaplar
+﻿int number1 = 2147483647; // int veri tipinin değer aralığı +-2147483647, 32bit yer kaplar
+long number2 = 9223372036854775807;// int veri tipinin iki katı değer aralığı +-, 64bit yer kaplar
 
 Console.WriteLine("Number is {0}", number1);
+Console.WriteLine("Number is {0}", number2);
 Console.ReadLine();
