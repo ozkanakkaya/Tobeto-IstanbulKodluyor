9006d0c822e6a794dafe3ec35dda67b6e82af59f
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 00:58:02 +0300

    Ref keyword ile çalışma yapıldı.

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -1,7 +1,12 @@
-﻿Add();
-Add();
-var result = Add2(10);
-Console.WriteLine(result);
+﻿//Add();
+//Add();
+//var result = Add2(10);
+
+int number1 = 20;
+int number2 = 100;
+var result2 = Add3(ref number1, number2); //ref keywordü değer tipleri referans tip gibi kullanabilmesi sağlar.
+Console.WriteLine(result2);
+Console.WriteLine(number1);
 
 Console.ReadLine();
 
@@ -14,4 +19,11 @@ static int Add2(int number1, int number2=30)//default değerler en sonda olmalı
 {
     var result = number1 + number2;
     return result;
+}
+
+static int Add3(ref int number1, int number2)
+{
+    number1 = 30;
+    return  number1 + number2;
+
 }
