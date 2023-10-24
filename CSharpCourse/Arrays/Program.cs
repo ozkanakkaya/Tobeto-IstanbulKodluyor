db7c2a5087ba82ea71866da810531a9f993d36bd
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 01:28:58 +0300

    Diziler ile çalışma yapıldı.

--- CSharpCourse/Arrays/Program.cs
+++ CSharpCourse/Arrays/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] students = new String[3] { "Özkan", "Özkan2", "Özkan3" };
+string[] studens2 = { "Özkan", "Özkan2", "Özkan3" };
+//studens2[3] = "Ahmet";
+
+foreach (var student in studens2)
+{
+    Console.WriteLine(student);
+}
+
+Console.WriteLine();
+Console.ReadLine();
