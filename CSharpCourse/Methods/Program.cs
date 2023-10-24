de915f5a15304310af8a2c9e1cea28501f021619
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-25 01:01:00 +0300

    Out keyword ile çalışmak

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -4,7 +4,8 @@
 
 int number1 = 20;
 int number2 = 100;
-var result2 = Add3(ref number1, number2); //ref keywordü değer tipleri referans tip gibi kullanabilmesi sağlar.
+/*var result2 = Add3(ref number1, number2);*/ //ref keywordü değer tipleri referans tip gibi kullanabilmesi sağlar.
+var result2 = Add3(out number1, number2); //reften farkı başlangıç değeri olması lazım ve değişken bir kere set edilmiş olması gerekir.
 Console.WriteLine(result2);
 Console.WriteLine(number1);
 
@@ -21,7 +22,7 @@ static int Add2(int number1, int number2=30)//default değerler en sonda olmalı
     return result;
 }
 
-static int Add3(ref int number1, int number2)
+static int Add3(out int number1, int number2)
 {
     number1 = 30;
     return  number1 + number2;
