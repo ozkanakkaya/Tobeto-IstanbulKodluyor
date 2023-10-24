83e5b2152529f85477afa939c28d9dda99bc7042
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-24 16:55:03 +0300

    Decimal veri tipi işlendi.

--- CSharpCourse/TypesAndVariables/Program.cs
+++ CSharpCourse/TypesAndVariables/Program.cs
@@ -5,13 +5,14 @@ byte number4 = 255; //0-255 arası değer alıyor, 32bit
 bool condition = true;// değer tiptedir.
 char character = 'A';
 double number5 = 10.4;//değer tiptedir. ondalıkla sayıları tutar, 64bit
+decimal number6 = 10.4m;//hassas verileri tutar, virgüllüyse m harfi yazılır, virgülden sonra en fazla 29 basamak, 8bit
 
 Console.WriteLine("Number1 is {0}", number1);
 Console.WriteLine("Number2 is {0}", number2);
 Console.WriteLine("Number3 is {0}", number3);
 Console.WriteLine("Number4 is {0}", number4);
 Console.WriteLine("Character is {0}", (int)character);//int'e çevirildi ve ascii kodunu verdi.
-Console.WriteLine("Number4 is {0}", number5);
-
+Console.WriteLine("Number5 is {0}", number5);
+Console.WriteLine("Number6 is {0}", number6);
 
 Console.ReadLine();
