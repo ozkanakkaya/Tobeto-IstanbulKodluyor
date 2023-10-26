3ccf6375078355503381a415527af6a5438c1de5
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-27 01:08:23 +0300

    Interfaceleri neden kullanıyoruz?

--- CSharpCourse/Interfaces/Program.cs
+++ CSharpCourse/Interfaces/Program.cs
@@ -1,4 +1,27 @@
-﻿interface IPerson
+﻿
+
+PersonManager manager = new PersonManager();
+Customer customer = new Customer
+{
+    Id = 1,
+    FirstName = "Özkan",
+    LastName = "Akkaya",
+    Address = "İstanbul"
+};
+
+Student student = new Student
+{
+    Id = 1,
+    FirstName = "Özkan1",
+    LastName = "Akkaya1",
+    Departmant = "Software",
+};
+
+manager.Add(customer);
+manager.Add(student);
+
+
+interface IPerson
 {
     int Id { get; set; }
     string FirstName { get; set; }
@@ -8,17 +31,32 @@
 
 class Customer : IPerson
 {
-    public int Id { get ; set ; }
+    public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Address { get; set; }
 }
 
-class Student: IPerson
+class Student : IPerson
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Departmant { get; set; }
+}
 
+class Worker : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant { get; set; }
+}
+
+class PersonManager
+{
+    public void Add(IPerson person)
+    {
+        Console.WriteLine(person.FirstName);
+    }
 }
