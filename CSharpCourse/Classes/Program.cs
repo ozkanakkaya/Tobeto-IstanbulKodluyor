f4399031dd7709e69303a5540d1bed02b534b07d
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-27 00:07:29 +0300

    Classlar içinde property tanımlamak

--- CSharpCourse/Classes/Program.cs
+++ CSharpCourse/Classes/Program.cs
@@ -8,4 +8,21 @@ ProductManager productManager= new ProductManager();
 productManager.Add();
 productManager.Update();
 
+
+Customer customer= new Customer();
+customer.Id = 1;
+customer.FirstName = "Özkan";
+customer.LastName = "Akkaya";
+customer.City = "İstanbul";
+
+Customer customer1 = new Customer
+{
+    Id = 2,
+    FirstName = "Özkan1",
+    LastName = "Akkaya1",
+    City = "Giresun"
+};
+
+Console.WriteLine(customer1.FirstName);
+
 Console.ReadLine();
