0073700631feeb3a2156330b779d72972ff370d3
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-27 00:56:34 +0300

    Interfacelere temel giriş

---
+++ CSharpCourse/Interfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿interface IPerson
+{
+    int Id { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }
+}
+
+
+class Customer : IPerson
+{
+    public int Id { get ; set ; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+}
+
+class Student: IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant { get; set; }
+
+}
