be6bc59029a0521d3353c0bbbe9154a5077d875b
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-26 23:59:03 +0300

    Classes projesi oluşturuldu. Classlara giriş yapıldı.

---
+++ CSharpCourse/Classes/Program.cs
@@ -0,0 +1,11 @@
+﻿using Classes;
+
+CustomerManager customerManager= new CustomerManager();
+customerManager.Add();
+customerManager.Update();
+
+ProductManager productManager= new ProductManager();
+productManager.Add();
+productManager.Update();
+
+Console.ReadLine();
