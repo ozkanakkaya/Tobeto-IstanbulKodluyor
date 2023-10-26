1da8b1feb031b75e2c77f8c4422cd192f21782dc
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-27 02:02:07 +0300

    Kalıtıma giriş

---
+++ CSharpCourse/Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿
+Person[] persons=new Person[3]
+{
+    new Customer
+    {
+        FirstName="Özkan"
+    },
+    new Student
+    {
+        FirstName="Özkan1"
+    },
+    new Person
+    {
+        FirstName="Özkan2"
+    }
+};
+
+foreach (var person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class Customer: Person
+{
+    public string City { get; set; }
+}
+
+class Student: Person
+{
+    public string Department { get; set; }
+}
