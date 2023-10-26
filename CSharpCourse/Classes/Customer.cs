23868b8cdb0296fd0f48bcc8dfbdbccb49ba0980
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-10-27 00:15:29 +0300

    Encapsulation

--- CSharpCourse/Classes/Customer.cs
+++ CSharpCourse/Classes/Customer.cs
@@ -10,6 +10,18 @@ namespace Classes
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
+        //private string _firstName;
+        //public string FirstName //Encapsulation
+        //{
+        //    get
+        //    {
+        //        return "Mr." + _firstName;
+        //    }
+        //    set
+        //    {
+        //        _firstName = value;
+        //    }
+        //}
         public string LastName { get; set; }
         public string City { get; set; }
 
