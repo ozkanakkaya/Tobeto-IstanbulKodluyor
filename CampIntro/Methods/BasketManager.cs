6fde3b87eddfdbd74d26c9173ea8ddb1be493c35
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-11-09 02:06:21 +0300

    Metotlar

---
+++ CampIntro/Methods/BasketManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class BasketManager
+    {
+        public void Ekle(Product urun)
+        { 
+            Console.WriteLine("Tebrikler. Sepete eklendi"); 
+        }
+    }
+}
---
+++ CampIntro/Methods/Program.cs
@@ -0,0 +1,31 @@
+﻿using Methods;
+using System.Text;
+
+string urunAdi = "Elma";
+double fiyati = 15;
+string aciklama = "Amasya elmasi";
+string[] meyveler = new string[] { };
+
+Product urun1 = new Product();
+urun1.Adi = "Elma";
+urun1.Fiyati = 15;
+urun1.Aciklama = "Amasya elmasi";
+
+Product urun2 = new Product();
+urun2.Adi = "Karpuz";
+urun2.Fiyati = 80;
+urun2.Aciklama = "Diyarbakir karpuzu";
+
+Product[] urunler = new Product[] { urun1, urun2 };
+
+foreach (var urun in urunler)
+{
+    Console.WriteLine(urun.Adi);
+    Console.WriteLine(urun.Fiyati);
+    Console.WriteLine(urun.Aciklama);
+    Console.WriteLine("-------------------");
+}
+
+BasketManager basketManager = new BasketManager();
+basketManager.Ekle(urun1);
+basketManager.Ekle(urun2);
