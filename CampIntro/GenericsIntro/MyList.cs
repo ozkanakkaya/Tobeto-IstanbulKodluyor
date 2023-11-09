a56cdfb27b59497fce294dceac92d9885b2c2108
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-11-09 18:23:45 +0300

    List yerine kendi Koleksiyonumuzu oluşturduk

---
+++ CampIntro/GenericsIntro/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    public class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;//mevcut items'ı tutar
+            items = new T[items.Length + 1];//yeni bir referas oluşur ve eski items uçar. yani items'deki değerler boşalır.
+
+            for (int i = 0; i < tempArray.Length; i++)//items eksi değerlerine yeniden kavuşur.
+            {
+
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item; // items'ın son elemanına metoddan gelen item eklenir.
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
+
+        }
+        public T[] Items
+        {
+            get { return items; }
+        }
+    }
+}
---
+++ CampIntro/GenericsIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using GenericsIntro;
+
+MyList<string> isimler = new MyList<string>();
+isimler.Add("Engin");
+Console.WriteLine(isimler.Length); 
+isimler.Add("Kerem"); 
+Console.WriteLine(isimler.Length);
+
+foreach (var isim in isimler.Items)
+{
+    Console.WriteLine(isim);
+}
